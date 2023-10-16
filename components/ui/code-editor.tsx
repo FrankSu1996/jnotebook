ff5ee714da44ab03e56b66fe240c3f883127c598
Frank Su <franksu49@gmail.com>
2023-10-16 15:38:16 -0400

    monaco editor configured

--- components/ui/code-editor.tsx
+++ components/ui/code-editor.tsx
@@ -10,6 +10,20 @@ export const CodeEditor = () => {
     appliedTheme = theme === "dark" ? "vs-dark" : "light";
   }
   return (
-    <MonacoEditor height="500px" language="javascript" theme={appliedTheme} />
+    <MonacoEditor
+      height="500px"
+      language="javascript"
+      theme={appliedTheme}
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
   );
 };
