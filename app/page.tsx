7bfb8be6f7590fce2839c497e3e6c49fe1d08be0
Frank Su <franksu49@gmail.com>
2023-10-18 15:21:53 -0400

    refactored components

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,16 @@ import { Textarea } from "@/components/ui/textarea";
 import { fetchPlugin } from "@/plugins/fetch-plugin";
 import { CodeEditor } from "@/components/ui/code-editor";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { Preview } from "@/components/ui/preview";
 
 export default function Page() {
-  const iframeRef = useRef<any>();
   const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
   const esbuildInitialized = useRef<boolean>(false);
 
   const onClick = async () => {
     if (!esbuildInitialized.current) return;
-
-    // reset iframe html before bundling
-    iframeRef.current.srcdoc = html;
 
     const result = await esbuild.build({
       entryPoints: ["index.js"],
@@ -30,31 +28,9 @@ export default function Page() {
         global: "window",
       },
     });
-    iframeRef.current.contentWindow.postMessage(
-      result.outputFiles[0].text,
-      "*"
-    );
-  };
 
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch(err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-              console.error(err);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
+    setCode(result.outputFiles[0].text);
+  };
 
   const startService = async () => {
     if (!esbuildInitialized.current) {
@@ -78,21 +54,10 @@ export default function Page() {
           if (value) setInput(value);
         }}
       />
-      <Textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        color="blue"
-      ></Textarea>
       <div>
         <Button onClick={onClick}>Submit</Button>
       </div>
-      <iframe
-        title="code preview"
-        srcDoc={html}
-        sandbox="allow-scripts"
-        ref={iframeRef}
-        style={{ border: "1px solid" }}
-      ></iframe>
+      <Preview code={code} />
     </div>
   );
 }
---
+++ components/ui/preview.tsx
@@ -0,0 +1,44 @@
+import { useRef } from "react";
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch(err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
+
+export const Preview: React.FC<PreviewProps> = ({ code }) => {
+  console.log("rendered preview");
+  const iframeRef = useRef<any>();
+
+  if (iframeRef.current) {
+    iframeRef.current.srcdoc = html;
+    iframeRef.current.contentWindow.postMessage(code, "*");
+  }
+
+  return (
+    <iframe
+      title="code preview"
+      srcDoc={html}
+      sandbox="allow-scripts"
+      ref={iframeRef}
+    ></iframe>
+  );
+};
