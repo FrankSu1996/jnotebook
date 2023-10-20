162fdad5906a7c6dd9bfceb4d3e240f1ca5441b7
Frank Su <franksu49@gmail.com>
2023-10-20 07:32:57 -0400

    conditional rendering of md editor works

--- components/ui/text-editor.tsx
+++ components/ui/text-editor.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import MDEditor from "@uiw/react-md-editor";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const TextEditor: React.FC = () => {
   const [value, setValue] = useState<string | undefined>("**Hello world!!!**");
   const [editing, setEditing] = useState(false);
+  const mdEditorRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (mdEditorRef.current && event.target && mdEditorRef.current.contains(event.target as Node)) {
+        return;
+      }
       setEditing(false);
     };
     document.addEventListener("click", listener, { capture: true });
@@ -18,7 +22,7 @@ export const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div className="container">
+      <div className="container" ref={mdEditorRef}>
         <MDEditor value={value} onChange={setValue} />
       </div>
     );
