91a8f48de7c694587924488b3f6677222c99e491
Frank Su <franksu49@gmail.com>
2023-10-21 17:52:00 -0400

    fdsa

--- components/ui/Editor/cell-list.tsx
+++ components/ui/Editor/cell-list.tsx
@@ -25,6 +25,7 @@ export const CellList: React.FC = () => {
 
   return (
     <div>
+      {order.length === 0 ? <h1 className="text-center pb-6">Select a cell type to start your notebook.</h1> : null}
       <AddCell previousCellId={null} forceVisible={cellList.length === 0} />
       {renderedCells}
     </div>
