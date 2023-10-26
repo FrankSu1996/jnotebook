fab9a3225c45c29fccf91b8fb428a64a97765289
Frank Su <franksu49@gmail.com>
2023-10-26 12:28:32 -0400

    toolbar styling

--- components/ui/action-bar.tsx
+++ components/ui/action-bar.tsx
@@ -13,58 +13,60 @@ export const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
   const dispatch: AppDispatch = useDispatch();
 
   return (
-    <div className="relative bottom-0 left-0 z-20 group">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <Button
-              variant={"outline"}
-              size={"sm"}
-              onClick={() => dispatch(moveCell({ id, direction: "up" }))}
-              className="rounded-none opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-            >
-              <ChevronUp />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent align="center">
-            <p className="p-3 pl-10">Move Up (ctl+up)</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <Button
-              variant={"outline"}
-              size={"sm"}
-              onClick={() => dispatch(moveCell({ id, direction: "down" }))}
-              className="rounded-none opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-            >
-              <ChevronDown />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent align="center">
-            <p className="p-3">Move Down (ctl+down)</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <Button
-              variant={"destructive"}
-              size={"sm"}
-              onClick={() => dispatch(deleteCell(id))}
-              className="rounded-none opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-            >
-              <X />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent align="center">
-            <p className="p-3">Delete cell</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+    <div className="flex z-20 group bg-muted opacity-50 hover:opacity-75 transition-opacity duration-300 rounded">
+      <div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant={"outline"}
+                size={"sm"}
+                onClick={() => dispatch(moveCell({ id, direction: "up" }))}
+                className="opacity-50 group-hover:opacity-100 transition-opacity duration-300  rounded"
+              >
+                <ChevronUp />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent align="center">
+              <p className="p-3 pl-10">Move Up (ctl+up)</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant={"outline"}
+                size={"sm"}
+                onClick={() => dispatch(moveCell({ id, direction: "down" }))}
+                className="rounded opacity-50 group-hover:opacity-100 transition-opacity duration-300 "
+              >
+                <ChevronDown />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent align="center">
+              <p className="p-3">Move Down (ctl+down)</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant={"destructive"}
+                size={"sm"}
+                onClick={() => dispatch(deleteCell(id))}
+                className="rounded opacity-50 group-hover:opacity-100 transition-opacity duration-300 "
+              >
+                <X />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent align="center">
+              <p className="p-3">Delete cell</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </div>
   );
 };
